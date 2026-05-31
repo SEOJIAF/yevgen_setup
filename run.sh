44bbe5e14c5e4c45b979cd76b6733bd6f14dae9c
seojiaf <dhlavaty.jr@gmail.com>
2026-05-31 17:40:06 +0200

    re-rolled keys

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-export DOZZLE_PASSWORD='sexypes'
-export DOZZLE_EMAIL='SSH_USER@oryks.org'
-export DOZZLE_USERNAME='SSH_USER'
+set -a
+. ./.env
+set +a
+
 clear
 ansible-playbook -i inventory.ini playbook.yaml --ask-become-pass
