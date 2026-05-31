47973174fc254fac6a9d1a8ea42a7076db4a66cf
seojiaf <dhlavaty.jr@gmail.com>
2026-05-31 17:04:28 +0200

    it didnt

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-source .env
+export DOZZLE_PASSWORD='sexypes'
+export DOZZLE_EMAIL='SSH_USER@oryks.org'
+export DOZZLE_USERNAME='SSH_USER'
+clear
 ansible-playbook -i inventory.ini playbook.yaml --ask-become-pass
