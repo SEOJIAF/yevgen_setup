f4546dbb7ac678fbf61aa79ea4e159d7ee51f4aa
seojiaf <seojiaf@proton.me>
2026-07-03 08:01:45 +0200

    added emoji endpoint

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/bash
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_ed25519
 set -a
 . ./.env
 set +a
@@ -6,4 +8,8 @@ set +a
 clear
 ansible-galaxy role install -r requirements.yml --force
 ansible-galaxy collection install -r requirements.yml --force
+
+sleep 1
+clear
+
 ansible-playbook -i inventory.yml playbook.yaml --ask-become-pass
